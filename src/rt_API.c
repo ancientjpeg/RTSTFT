f22bae04e0da7c22444bc729c6f771c854aee5b1
Jackson Kaplan <JwyattK@gmail.com>
2022-02-05 15:41:03 -0800

    shuffled rt_API.c a bit

--- src/rt_API.c
+++ src/rt_API.c
@@ -2,38 +2,10 @@
 #define rt_max(a, b) ((a) > (b) ? (a) : (b))
 #define rt_min(a, b) ((a) < (b) ? (a) : (b))
 
-rt_chan rt_chan_init(rt_params p)
-{
-  rt_chan chan       = (rt_chan)malloc(sizeof(rt_chan_t));
-  chan->first_frame  = 1;
-  chan->framebuf     = rt_framebuf_init(p, 2);
-  chan->in           = rt_fifo_init(rt_max(p->buffer_size, p->frame_size * 2));
-  rt_uint lerp_frame = p->overlap_factor * p->hop_s + p->frame_size;
-  chan->pre_lerp     = rt_fifo_init(
-          rt_max((rt_uint)ceil((rt_real)p->buffer_size * 2 * p->scale_factor),
-                 lerp_frame * 2));
-  chan->out = rt_fifo_init(rt_max(p->buffer_size, p->frame_size * 2));
-  chan->plan =
-      fftw_plan_r2r_1d(p->fft_size, chan->framebuf->frames[0],
-                       chan->framebuf->frames[0], FFTW_R2HC, FFTW_ESTIMATE);
-  chan->plan_inv =
-      fftw_plan_r2r_1d(p->fft_size, chan->framebuf->frames[0],
-                       chan->framebuf->frames[0], FFTW_HC2R, FFTW_ESTIMATE);
-  return chan;
-}
+rt_chan rt_chan_init(rt_params p);
+rt_chan rt_chan_clean(rt_chan chan);
 
-rt_chan rt_chan_clean(rt_chan chan)
-{
-  chan->framebuf = rt_framebuf_destroy(chan->framebuf);
-  chan->in       = rt_fifo_destroy(chan->in);
-  chan->pre_lerp = rt_fifo_destroy(chan->pre_lerp);
-  fftw_destroy_plan(chan->plan);
-  fftw_destroy_plan(chan->plan_inv);
-  free(chan);
-  return (rt_chan)NULL;
-}
-
-void rt_cycle_single(rt_params p, rt_real *buffer, rt_uint buffer_len)
+void    rt_cycle_single(rt_params p, rt_real *buffer, rt_uint buffer_len)
 {
   rt_cycle_chan(p, 0, buffer, buffer_len);
 }
@@ -102,6 +74,37 @@ rt_params rt_clean(rt_params p)
   free(p->chans);
   free(p);
   return (rt_params)NULL;
+}
+
+rt_chan rt_chan_init(rt_params p)
+{
+  rt_chan chan       = (rt_chan)malloc(sizeof(rt_chan_t));
+  chan->first_frame  = 1;
+  chan->framebuf     = rt_framebuf_init(p, 2);
+  chan->in           = rt_fifo_init(rt_max(p->buffer_size, p->frame_size * 2));
+  rt_uint lerp_frame = p->overlap_factor * p->hop_s + p->frame_size;
+  chan->pre_lerp     = rt_fifo_init(
+          rt_max((rt_uint)ceil((rt_real)p->buffer_size * 2 * p->scale_factor),
+                 lerp_frame * 2));
+  chan->out = rt_fifo_init(rt_max(p->buffer_size, p->frame_size * 2));
+  chan->plan =
+      fftw_plan_r2r_1d(p->fft_size, chan->framebuf->frames[0],
+                       chan->framebuf->frames[0], FFTW_R2HC, FFTW_ESTIMATE);
+  chan->plan_inv =
+      fftw_plan_r2r_1d(p->fft_size, chan->framebuf->frames[0],
+                       chan->framebuf->frames[0], FFTW_HC2R, FFTW_ESTIMATE);
+  return chan;
+}
+
+rt_chan rt_chan_clean(rt_chan chan)
+{
+  chan->framebuf = rt_framebuf_destroy(chan->framebuf);
+  chan->in       = rt_fifo_destroy(chan->in);
+  chan->pre_lerp = rt_fifo_destroy(chan->pre_lerp);
+  fftw_destroy_plan(chan->plan);
+  fftw_destroy_plan(chan->plan_inv);
+  free(chan);
+  return (rt_chan)NULL;
 }
 
 rt_uint rt_real_size() { return sizeof(rt_real); }
