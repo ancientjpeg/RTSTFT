dde8b9e35a6e63c57e97ae84b78f8966fa9aecd3
Jackson Kaplan <JwyattK@gmail.com>
2022-03-20 12:08:11 -0700

    doubling input block size

--- src/rt_chan.c
+++ src/rt_chan.c
@@ -13,9 +13,9 @@
 rt_chan rt_chan_init(rt_params p)
 {
   rt_chan chan       = (rt_chan)malloc(sizeof(rt_chan_t));
-  chan->in           = rt_fifo_init(rt_max(p->buffer_size, p->fft_max * 2));
+  chan->in           = rt_fifo_init(rt_max(p->buffer_size * 2, p->fft_max * 2));
   rt_uint padded_out = ceil(p->fft_max * 2 * p->scale_factor_max);
-  chan->out          = rt_fifo_init(rt_max(p->buffer_size, padded_out));
+  chan->out          = rt_fifo_init(rt_max(p->buffer_size * 2, padded_out));
   chan->manip        = rt_manip_init(p);
   chan->framebuf     = rt_framebuf_init(p);
   return chan;
